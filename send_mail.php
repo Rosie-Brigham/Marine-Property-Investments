35e6d69c51d6cdb7caf637727ad3f1fda4c94e8b
Rosie Brigham <zcwcg03@live.ucl.ac.uk>
2013-08-25 16:35:38 +0100

    form?

--- send_mail.php
+++ send_mail.php
@@ -3,22 +3,22 @@
 This first bit sets the email address that you want the form to be submitted to.
 You will need to change this value to a valid email address that you can access.
 */
-webmaster_email = "info@marinepropertyinvestments.co.uk";
+$webmaster_email = "name@example.com";
 
 /*
 This bit sets the URLs of the supporting pages.
 If you change the names of any of the pages, you will need to change the values here.
 */
-feedback_page = "feedback_form.html";
-error_page = "error_message.html";
-thankyou_page = "thank_you.html";
+$feedback_page = "feedback_form.html";
+$error_page = "error_message.html";
+$thankyou_page = "thank_you.html";
 
 /*
 This next bit loads the form field data into variables.
 If you add a form field, you will need to add it here.
 */
-email_address = $_REQUEST['email_address'] ;
-comments = $_REQUEST['comments'] ;
+$email_address = $_REQUEST['email_address'] ;
+$comments = $_REQUEST['comments'] ;
 
 /*
 The following function checks for email injection.
@@ -61,7 +61,7 @@ header( "Location: $error_page" );
 // If we passed all previous tests, send the email then redirect to the thank you page.
 else {
 mail( "$webmaster_email", "Feedback Form Results",
-  comments, "From: $email_address" );
+  $comments, "From: $email_address" );
 header( "Location: $thankyou_page" );
 }
 ?>
